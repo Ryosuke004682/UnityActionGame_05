72ca48f7b4a82259c822708299aef03976512cae
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-07-05 15:17:45 +0900

    敵のチェイス範囲を作成

--- My_ActionGame/Assets/Script/Enemy/EnemyStateMachine.cs
+++ My_ActionGame/Assets/Script/Enemy/EnemyStateMachine.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class EnemyStateMachine : StateMachine
 {
-    [field: SerializeField] public Animator Animator { get; private set; }
+    [field: SerializeField] public Animator              Animator { get; private set; }
+    [field: SerializeField] public CharacterController Controller { get; private set; }
+    [field: SerializeField] public ForceReceiver         Receiver { get; private set; }
+
+    [field: SerializeField] public float PlayerChasingRange { get; private set; }
+
+    public GameObject Player { get; private set; }
 
     private void Start()
     {
+        Player = GameObject.FindGameObjectWithTag("Player");
+
         SwitchState(new EnemyIdleState(this));
+    }
+
+    //索敵範囲を視覚的に表す。（デバッグが楽だから）
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position , PlayerChasingRange);
     }
 }
